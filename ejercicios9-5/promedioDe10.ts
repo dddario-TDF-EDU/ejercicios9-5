71f1bada4e5c853c25b0dd8f4cdccc7c9331c767
dddario-TDF-EDU <103612977+dddario-TDF-EDU@users.noreply.github.com>
2022-05-14 21:36:25 -0300

    promedio10notas en progreso buscandole la vuelta a esta cosa...

--- ejercicios9-5/promedioDe10.ts
+++ ejercicios9-5/promedioDe10.ts
@@ -3,14 +3,16 @@ let botonaso = document.getElementById("comenzar");
 let i: number = 0;
 let sumaNotas: number = 0;
 
-botonaso.addEventListener("click", () => {
+
+ botonaso.addEventListener("click", () => {
   let auxNotas: number = Number(espacioNota.value);
-
-  i++;
-
   while (i != 5) {
+    
+ 
     sumaNotas = auxNotas + sumaNotas;
+    i++;
+    
   }
-
-  console.log(sumaNotas);
-});
+console.log(sumaNotas);
+    });
+  
