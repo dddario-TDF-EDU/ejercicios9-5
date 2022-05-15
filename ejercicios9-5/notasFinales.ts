9538ed2bb805470e1a7213d4b7cb8d3e9c4e4e08
dddario-TDF-EDU <103612977+dddario-TDF-EDU@users.noreply.github.com>
2022-05-14 22:45:02 -0300

    ejercicio notas final sin terminar

---
+++ ejercicios9-5/notasFinales.ts
@@ -0,0 +1,7 @@
+let botonaso = document.getElementById("comenzar");
+
+botonaso?.addEventListener("click", () => {
+  document.getElementById("app")?.innerHTML = "cola loca";
+});
+
+//la primer nota se multiplica por 0.1, la segunda por 0,5 y la tercera por 0,4
--- ejercicios9-5/promedioDe10.ts
+++ ejercicios9-5/promedioDe10.ts
@@ -3,16 +3,23 @@ let botonaso = document.getElementById("comenzar");
 let i: number = 0;
 let sumaNotas: number = 0;
 
-
- botonaso.addEventListener("click", () => {
-  let auxNotas: number = Number(espacioNota.value);
+botonaso.addEventListener("click", () => {
   while (i != 5) {
-    
- 
+    let auxNotas: number = Number(prompt("Ingrese notas:"));
     sumaNotas = auxNotas + sumaNotas;
     i++;
-    
   }
-console.log(sumaNotas);
-    });
-  
+  console.log(sumaNotas);
+  console.log("preview " + sumaNotas);
+  let promedio: number = sumaNotas / i;
+  alert("el promedio de notas es: " + promedio);
+});
+
+//for ( notas =0 ; notas<=3; notas++){
+
+//let notasTotal:number=0;
+//let notaAux:number=Number(prompt("Ingrese notas:";"nota"));
+//let notasTotal=notasTotal+notaAux;
+//console.log(notaAux)
+
+//}
