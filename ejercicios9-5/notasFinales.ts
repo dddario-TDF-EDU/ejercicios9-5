e235ca3b4f2cc28b1fc931fbda27f2d6b2bc1fa9
dddario-TDF-EDU <103612977+dddario-TDF-EDU@users.noreply.github.com>
2022-05-17 01:27:55 -0300

    notas Finales ya es funcional A esperar de ver errores a futuro, en este momento funciona. Queda revisar como cambiar el valor de las variables enviadas como parametros.

--- ejercicios9-5/notasFinales.ts
+++ ejercicios9-5/notasFinales.ts
@@ -1,68 +1,78 @@
 let botonaso = document.getElementById("comenzar");
 
-function claseNota(nota: number, tipoNota: number) {
+function claseNota(nota: number, tipoNota: number): number {
   switch (tipoNota) {
     case 1:
       nota = nota * 0.1;
-      break;
+      return nota;
     case 2:
       nota = nota * 0.5;
-      break;
+      return nota;
     case 3:
       nota = nota * 0.4;
-      break;
+      return nota;
     default:
       console.log("no debio haberse llegado a este punto, corregir");
+      return 0;
   }
 }
 
-function verificacionNota(nota: number, tipoNota: number, notaValida: boolean) {
+function verificacionNota(nota: number, tipoNota: number): number {
   let notaAuxLocal = Number(prompt("Ingrese la nota:"));
 
   if (notaAuxLocal < 11 && notaAuxLocal >= 0) {
-    claseNota(notaAuxLocal, tipoNota);
-    nota = notaAuxLocal;
+    notaAuxLocal = claseNota(notaAuxLocal, tipoNota);
   } else {
     alert("Nota ingresada invalida, pase al siguiente estudiante");
-    notaValida = false;
+    notaAuxLocal = -1;
   }
+  return notaAuxLocal;
 }
 
-function verificacionNombre(finalizarCiclo: boolean) {
+function verificacionNombre(finCiclo: boolean): boolean {
+  finCiclo = false;
   let nombreAux: string = prompt("ingrese el nombre del estudiante");
   if (nombreAux != "") {
     alert("Nombre valido, proceda a cargar los datos");
   } else {
     alert("Fin del ciclo, comienza calculo del promedio.");
-    finalizarCiclo = true;
+    finCiclo = true;
   }
+  return finCiclo;
 }
 
 botonaso.addEventListener("click", () => {
   let finCiclo: boolean = false;
-  let notaValida: boolean = true;
   let notaAux: number = 0;
   let contadorVueltas: number = 0;
   let notaMedioCiclo: number = 0;
   let notaTotal: number = 0;
 
-  while (finCiclo != true) {
-    verificacionNombre(finCiclo);
-    if (finCiclo != true) {
-      for (let i: number = 1; i < 4; i++) {
-        verificacionNota(notaAux, i, notaValida);
-        if (notaValida == true) {
+  while (finCiclo !== true) {
+    // if (finCiclo == true) {
+    //   alert("comprobado, este while no funciona , es " + finCiclo);
+    // }
+    finCiclo = verificacionNombre(finCiclo);
+    //alert("comprobado, este while no funciona , es " + finCiclo);
+    if (finCiclo == false) {
+      for (let cantNotas: number = 1; cantNotas < 4; cantNotas++) {
+        notaAux = verificacionNota(notaAux, cantNotas);
+        if (notaAux != -1) {
           notaMedioCiclo = notaMedioCiclo + notaAux;
         } else {
           notaMedioCiclo = 0;
           break;
         }
       }
-      notaTotal = notaTotal + notaMedioCiclo;
+    }
+    notaTotal = notaTotal + notaMedioCiclo;
+    if (notaMedioCiclo != 0) {
       contadorVueltas++;
     }
+    notaMedioCiclo = 0;
   }
-
+  console.log("estado " + contadorVueltas);
+  console.log("estado " + notaTotal);
   alert("el promedio final de notas es: " + notaTotal / contadorVueltas);
 });
 
